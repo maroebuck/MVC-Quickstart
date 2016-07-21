7ffd4090333948a8817d76bd278311dbe6db3ce7
Mark Roebuck <mark@bifrost.pro>
2016-07-21 02:04:47 +0100

    Initial commit

---
+++ MVC.Quickstart/src/MVC.Quickstart/wwwroot/app/app.component.ts
@@ -0,0 +1,7 @@
+﻿import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: '<h1>Angular 2 and ASP.NET Core MVC Quickstart</h1>'
+})
+export class AppComponent { }
---
+++ MVC.Quickstart/src/MVC.Quickstart/wwwroot/systemjs.config.js
@@ -0,0 +1,61 @@
+﻿(function (global) {
+
+    var ngVer = '@2.0.0-rc.4';
+    var routerVer = '@3.0.0-beta.1';
+    var formsVer = '@0.2.0';
+
+    var map = {
+        'app': 'app',
+
+        '@angular': 'https://npmcdn.com/@angular',
+        '@angular/router': 'https://npmcdn.com/@angular/router' + routerVer,
+        '@angular/forms': 'https://npmcdn.com/@angular/forms' + formsVer,
+        'rxjs': 'https://npmcdn.com/rxjs@5.0.0-beta.6',
+        'ts': 'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
+        'typescript': 'https://npmcdn.com/typescript@1.8.10/lib/typescript.js'
+    };
+
+    var packages = {
+        'app': { main: 'main.js', defaultExtension: 'js' },
+        'rxjs': { defaultExtension: 'js' }
+    };
+
+    var ngPackageNames = [
+      'common',
+      'compiler',
+      'core',
+      'http',
+      'platform-browser',
+      'platform-browser-dynamic',
+      'upgrade'
+    ];
+
+    ngPackageNames.forEach(function (pkgName) {
+        map['@angular/' + pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
+    });
+
+    ngPackageNames.forEach(function (pkgName) {
+
+        packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+
+        packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    });
+
+    packages['@angular/router'] = { main: 'index.js', defaultExtension: 'js' };
+
+    packages['@angular/forms'] = { main: 'index.js', defaultExtension: 'js' };
+
+
+    var config = {
+        meta: {
+            'typescript': {
+                "exports": "ts"
+            }
+        },
+        map: map,
+        packages: packages
+    };
+
+    System.config(config);
+
+})(this);
